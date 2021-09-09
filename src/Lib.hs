d62c380b573f32413de89c0785f50725ec540a18
Dane Lipscombe <dane@lipscombe.com.au>
2021-09-09 20:13:19 +1000

    Use hspec

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,3 +5,7 @@ double x = x * 2
 
 listComprehension :: [Integer]
 listComprehension = [a | a <- [1,2,3], a < 3]
+
+prod :: Num p => [p] -> p
+prod [] = 1
+prod (x:xs) = x * (prod xs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,11 @@
 import Lib
+import Test.Hspec
+import Control.Exception (evaluate)
 
 main :: IO ()
-main = do
-  putStrLn ""
-  putStr "double "
-  putStrLn $ if double 1 == 2 then "OK" else "FAIL!"
-  putStr "listComprehension "
-  putStrLn $ if listComprehension == [1,2] then "OK" else "FAIL!"
-  return ()
+main = hspec $ do
+  describe "Lib" $ do
+    it "doubles ok" $ double 1 `shouldBe` 2
+    it "listComprehension works" $ listComprehension `shouldBe` [1,2]
+    it "sums" $ sum [1] `shouldBe` 1
+    it "calculates products" $ prod [5, 10] `shouldBe` 50
